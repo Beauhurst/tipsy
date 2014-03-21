8411936551fe6038a1dfeeec4338020d4863cfe5
Robert Scott <code@humanleg.org.uk>
2014-03-21 14:14:37 +0000

    change detection of "element not in DOM" on show to work properly in IE 8&9

--- src/javascripts/jquery.tipsy.js
+++ src/javascripts/jquery.tipsy.js
@@ -26,7 +26,7 @@
     Tipsy.prototype = {
         show: function() {
             var title = this.getTitle();
-            if ( title && this.enabled && $.contains ( document , this.$element[0] ) ) {
+            if ( title && this.enabled && this.$element.closest ( document.documentElement ).length ) {
                 // ^ also ensuring this.$element is still in the DOM. If not, we're not wanted.
                 // emit a useful event
                 this.$element.trigger ( 'tipsyPreShow' );
